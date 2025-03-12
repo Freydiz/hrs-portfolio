0af4bf45ef86251e2fdb742f0664bb1717843080
Henriette Riis Steinbach <hrs@MacBook-Pro-tilhrende-Henriette.local>
2025-03-12 11:01:31 +0100

    rename file

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -38,7 +38,7 @@ const Hero: React.FC<HeroProps> = ({ slice }) => {
           transformOrigin: "left top",
           delay: 0.5,
           stagger: { each: 0.1, from: "random" },
-        }
+        },
       );
 
       tl.fromTo(
@@ -54,7 +54,7 @@ const Hero: React.FC<HeroProps> = ({ slice }) => {
           scale: 1,
           ease: "elastic.out(1, 0.3)",
           duration: 1,
-        }
+        },
       );
     }, ref);
 
@@ -79,16 +79,17 @@ const Hero: React.FC<HeroProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={ref}
+      className="h-dvh"
     >
-      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+      <div className="grid grid-cols-1 items-center md:grid-cols-2">
         <LogoCanvas />
 
         <div className="col-start-1 md:row-start-1">
           <h1
-            className="mb-8 text-[clamp(3rem,12vmin,20rem)] font-extrabold leading-none tracking-tighter"
+            className="mb-8 text-[clamp(3rem,12vmin,20rem)] leading-none font-extrabold tracking-tighter"
             aria-label={`${slice.primary.firstname} ${slice.primary.middelname} ${slice.primary.lastname}`}
           >
-            <span className="inline-flex whitespace-nowrap gap-2">
+            <span className="inline-flex gap-2 whitespace-nowrap">
               <span>{renderLetters(slice.primary.firstname, "first")}</span>
               <span>{renderLetters(slice.primary.middelname, "middle")}</span>
             </span>
@@ -97,7 +98,7 @@ const Hero: React.FC<HeroProps> = ({ slice }) => {
             </span>
           </h1>
 
-          <span className="tag-line block bg-gradient-to-tr from-[var(--secondary)] via-[var(--accent)] to-[var(--secondary)] bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent md:text-4xl">
+          <span className="tag-line block bg-gradient-to-tr from-[var(--secondary)] via-[var(--accent)] to-[var(--secondary)] bg-clip-text text-2xl font-bold tracking-[.2em] text-transparent uppercase md:text-4xl">
             {slice.primary.tag_line}
           </span>
         </div>

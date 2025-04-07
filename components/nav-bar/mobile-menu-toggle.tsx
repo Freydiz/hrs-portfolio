c3d782fe7305d3de0ed7e943b16023b02fbf97fe
Freydiz <hennyriis@hotmail.com>
2025-04-07 22:36:19 +0200

    remove barrel import

--- components/nav-bar/mobile-menu-toggle.tsx
+++ components/nav-bar/mobile-menu-toggle.tsx
@@ -7,32 +7,12 @@ export const MobileMenuToggle: React.FC<Props> = (props) => {
   const { isMobileMenuOpen, toggleMobileMenu } = props;
 
   return (
-    <button
-      onClick={toggleMobileMenu}
-      aria-label="Toggle mobile menu"
-      className="p-2 focus:outline-none"
-    >
-      <svg
-        className="h-6 w-6"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+    <button onClick={toggleMobileMenu} aria-label='Toggle mobile menu' className='p-2 focus:outline-none'>
+      <svg className='h-6 w-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
         {isMobileMenuOpen ? (
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M6 18L18 6M6 6l12 12' />
         ) : (
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16m-7 6h7' />
         )}
       </svg>
     </button>
--- components/nav-bar/nav-bar.tsx
+++ components/nav-bar/nav-bar.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { Content } from "@prismicio/client";
-import { PrismicNextLink } from "@prismicio/next";
-import Link from "next/link";
-import { useState } from "react";
+import { Content } from '@prismicio/client';
+import { PrismicNextLink } from '@prismicio/next';
+import Link from 'next/link';
+import { useState } from 'react';
 
-import { MobileMenuToggle } from "./mobile-menu-toggle";
+import { MobileMenuToggle } from './mobile-menu-toggle';
 
 interface NavBarProps {
   settings: Content.SettingsDocument;
@@ -22,48 +22,30 @@ export const NavBar: React.FC<NavBarProps> = (props) => {
   };
 
   return (
-    <div className="container mx-auto flex h-16 items-center justify-between px-4">
-      <Link
-        href="/"
-        aria-label="Home page"
-        className="hover:text-accent text-xl font-extrabold tracking-tighter"
-      >
+    <div className='container mx-auto flex h-16 items-center justify-between px-4'>
+      <Link href='/' aria-label='Home page' className='hover:text-accent text-xl font-extrabold tracking-tighter'>
         {settings.data.name}
       </Link>
 
       {/* Desktop Navigation */}
-      <nav aria-label="Desktop Navigation" className="hidden md:flex md:gap-6">
+      <nav aria-label='Desktop Navigation' className='hidden md:flex md:gap-6'>
         {navItems.map(({ link, label }, index) => (
-          <PrismicNextLink
-            key={index}
-            field={link}
-            className="hover:text-accent"
-          >
+          <PrismicNextLink key={index} field={link} className='hover:text-accent'>
             {label}
           </PrismicNextLink>
         ))}
       </nav>
 
       {/* Mobile Navigation */}
-      <div className="flex items-center justify-between md:hidden">
-        <MobileMenuToggle
-          isMobileMenuOpen={mobileMenuOpen}
-          toggleMobileMenu={toggleMobileMenu}
-        />
+      <div className='flex items-center justify-between md:hidden'>
+        <MobileMenuToggle isMobileMenuOpen={mobileMenuOpen} toggleMobileMenu={toggleMobileMenu} />
       </div>
 
       {mobileMenuOpen && (
-        <div className="bg-primary/95 absolute inset-x-0 top-16 z-50 p-4 md:hidden">
-          <nav
-            aria-label="Mobile Navigation"
-            className="flex flex-col items-center space-y-4"
-          >
+        <div className='bg-primary/95 absolute inset-x-0 top-16 z-50 p-4 md:hidden'>
+          <nav aria-label='Mobile Navigation' className='flex flex-col items-center space-y-4'>
             {navItems.map(({ link, label }, index) => (
-              <PrismicNextLink
-                key={index}
-                field={link}
-                className="hover:text-accent"
-              >
+              <PrismicNextLink key={index} field={link} className='hover:text-accent'>
                 {label}
               </PrismicNextLink>
             ))}
